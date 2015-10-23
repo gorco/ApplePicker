2bd8449e79cde1b9fc5235960ca0cdacaec373d9
gorco <antcal01@ucm.es>
2015-10-23 17:27:00 +0200

    Added score and highScore

--- Assets/Apple.cs
+++ Assets/Apple.cs
@@ -4,11 +4,16 @@ using System.Collections;
 public class Apple : MonoBehaviour {
 
     public static float bottomY = -20f;
+
     void Update()
     {
         if (transform.position.y < bottomY)
         {
             Destroy(this.gameObject);
+            // Get a reference to the ApplePicker component of Main Camera
+            ApplePicker apScript = Camera.main.GetComponent<ApplePicker>();
+            // Call the public AppleDestroyed() method of apScript
+            apScript.AppleDestroyed();
         }
     }
 }
--- Assets/Basket.cs
+++ Assets/Basket.cs
@@ -2,6 +2,18 @@
 using System.Collections;
 public class Basket : MonoBehaviour
 {
+    public GUIText scoreGT;
+
+    void Start()
+    {
+        // Find a reference to the ScoreCounter GameObject
+        GameObject scoreGO = GameObject.Find("ScoreCounter");
+        // Get the GUIText Component of that GameObject
+        scoreGT = scoreGO.GetComponent<GUIText>();
+        // Set the starting number of points to 0
+        scoreGT.text = "0";
+    }
+
     void Update()
     {
         // Get the current screen position of the mouse from Input
@@ -21,12 +33,24 @@ public class Basket : MonoBehaviour
 
     void OnCollisionEnter(Collision coll)
     {
-        Debug.Log("MANZANA3D");
         // Find out what hit this basket
         GameObject collidedWith = coll.gameObject;
         if (collidedWith.tag == "Apple")
         {
             Destroy(collidedWith);
+        }
+
+        // Parse the text of the scoreGT into an int
+        int score = int.Parse(scoreGT.text);
+        // Add points for catching the apple
+        score += 100;
+        // Convert the score back to a string and display it
+        scoreGT.text = score.ToString();
+
+        // Track the high score
+        if (score > HighScore.score)
+        {
+            HighScore.score = score;
         }
     }
 
